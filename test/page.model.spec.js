f3d28d8c7938d3ab76818c48e6b7c0ec09f9058e
Alec Friedman <alec.friedman3@gmail.com>
2016-06-21 16:56:46 -0400

    error testing

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -2,6 +2,12 @@ var expect = require('chai').expect;
 var chai = require('chai');
 var spies = require('chai-spies');
 chai.use(spies);
+var Sequelize = require('sequelize');
+var marked = require('marked');
+
+var db = new Sequelize('postgres://localhost:5432/wikistackTest', {
+    logging: false
+});
 
 var DatabaseEditor = require('../models');
 var Page = require('../models').Page
@@ -11,17 +17,23 @@ describe( 'Page Model', function () {
   var page;
   beforeEach(function(){
     page = Page.build({
-      title: 'here is a title',
-      content: 'here lies the content',
-      urlTitle: 'thisURL'
+      title: 'title',
+      content: 'content'
     });
   })
 
   describe('Errors on fields', function (){
 
-    xit('Returns an error if no title', function () {
+    it('Returns an error if no title', function (done) {
       // Page.build();
-      // expect(Page.create({}).then(function(){})).to.throw(new Error());
+      page.save().then(function(){
+        console.log('in the promise');
+        done();
+      }).catch(function(err){
+        console.error(err.message);
+        expect(err.message).to.match(/title cannot be null/);
+        done()
+      })
     });
 
     xit('Returns an error if no urlTitle', function () {
@@ -35,13 +47,18 @@ describe( 'Page Model', function () {
 
   describe('Virtual methods', function(){
     describe('Route', function(){
-      it('returns /wiki/ + urlTitle', function (){
+      xit('returns /wiki/ + urlTitle', function (){
         expect(page.route).to.equal('/wiki/' + page.urlTitle)
       });
     });
     describe('Class Methods: findByTag', function (){
-      it('returns all entries containing tag', function (){
-        expect(page.findByTag).to.be.instanceOf(Promise)
+      xit('returns all entries containing tag', function (done){
+
+        Page.findByTag('string').then(function(pages){
+          expect(pages).to.have.lengthOf(0)
+          done();
+        });
+
       });
     });
     describe('instance method: findSimilar', function (){
