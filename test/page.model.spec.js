056007620b02e0b97af77f08c58b01d91a78c4c5
Richard Chao <r.chao03@gmail.com>
2016-06-22 09:36:32 -0400

    Wed Morning

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -5,58 +5,66 @@ chai.use(spies);
 var Sequelize = require('sequelize');
 var marked = require('marked');
 
-var db = new Sequelize('postgres://localhost:5432/wikistackTest', {
-    logging: false
-});
+// var db = new Sequelize('postgres://localhost:5432/wikistackTest', {
+//     logging: false
+// });
 
 var DatabaseEditor = require('../models');
 var Page = require('../models').Page
 var User = DatabaseEditor.User;
+var page
+describe('Page Model', function() {
+    beforeEach(function() {
+        page = Page.build({
+                title: "THIS IS TITLE!",
+                content: "HERE IS CONTENT!",
+                tags: ["one tag", "two tags", "three"]
+            })
+            // .then (function (){
 
-describe( 'Page Model', function () {
-  var page;
-  beforeEach(function(){
-    page = Page.build({
-      content: "content"
-     });
-  })
+        // })
+        // .catch(done)
+    })
 
-  describe('Errors on fields', function (){
 
-    it('Returns an error if no title', function (done) {
+    describe('Testing Properties', function() {
 
-    });
-
-    xit('Returns an error if no urlTitle', function (done) {
-
-    });
-
-    xit('Returns an error if no content', function (done) {
-
-    });
-  });
-
-  describe('Virtual methods', function(){
-    describe('Route', function(){
-      xit('returns /wiki/ + urlTitle', function (){
-        expect(page.route).to.equal('/wiki/' + page.urlTitle)
-      });
-    });
-    describe('Class Methods: findByTag', function (){
-      xit('returns all entries containing tag', function (done){
-
-        Page.findByTag('string').then(function(pages){
-          expect(pages).to.have.lengthOf(0)
-          done();
+        it('Returns no error if title is present', function() {
+            expect(page.title).to.equal("THIS IS TITLE!")
         });
 
-      });
-    });
-    describe('instance method: findSimilar', function (){
-      xit('returns all pages with similar ID or Tag', function (){
+        // xit('Returns no error if urlTitle is present', function () {
 
-      });
+        // });
+
+        it('Returns no error if content is present', function() {
+            expect(page.content).to.equal("HERE IS CONTENT!")
+        });
     });
-  });
+
+    describe('Virtual methods', function() {
+        describe('Route', function() {
+            it('returns /wiki/ + urlTitle', function() {
+                page.urlTitle = "something"
+                expect(page.route).to.equal('/wiki/' + page.urlTitle)
+            });
+        });
+        describe('Class Methods: findByTag', function() {
+            it('returns all entries containing tag', function(done) {
+
+                Page.findByTag('two tags').then(function(pages) {
+                  console.log(pages.dataValues)
+                    expect(pages.dataValues).to.have.lengthOf(1)
+                    done();
+                });
+
+            });
+        });
+        describe('instance method: findSimilar', function() {
+            xit('returns all pages with similar ID or Tag', function() {
+
+            });
+        });
+    });
 
 });
